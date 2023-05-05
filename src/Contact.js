1a12f1cac5217652c344b0a551bc7f1a1b5beb28
Mike F <92463194+Mfos234@users.noreply.github.com>
2023-05-04 22:16:42 -0400

    Initial commit

---
+++ src/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Me!</h1>
+      <p>
+        If you are interested in working with me or have any questions, please
+        feel free to contact me. You can reach me by email. I look forward to
+        hearing from you soon.
+      </p>
+    </div>
+  );
+}
