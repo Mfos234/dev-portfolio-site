f1e57c26726ed4d589547529c9ded625e0ef8c4c
Mike F <92463194+Mfos234@users.noreply.github.com>
2023-05-06 17:04:08 -0400

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
       <h1>Contact Me!</h1>
       <p>
         If you are interested in working with me or have any questions, please
-        feel free to contact me. You can reach me by <a href="michaelfoster.dev@gmail.com">e-mail</a>. I look forward to
+        feel free to contact me. You can reach me by <a href="mailto:michaelfoster.dev@gmail.com">e-mail</a>. I look forward to
         hearing from you soon.
       </p>
     </div>
